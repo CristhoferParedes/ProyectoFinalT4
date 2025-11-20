c9cbc23132b60aa981e49c95c2a0f5a6599c3124
Gacela <cristhoferparedes06@gmail.com>
2025-11-19 20:59:48 -0500

    Crear Proyecto

---
+++ SistemaVentas/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BibliotecaSistema;
+
+namespace SistemaVentas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola World");
+            Class1.n();
+        }
+    }
+}
